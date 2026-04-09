74b6f0d9e6abeacc411f0d97f701a3aa8cc41d2e
vaastikthegreat <vaastikthegreat@gmail.com>
2026-04-09 19:19:01 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ const Navbar = () => {
           Mega<span className="text-emerald-600">Fiber</span>
         </Link> */}
         <Link to="/" className="flex items-center text-2xl font-bold text-slate-900">
-          <img src="mega-fiber-logo.png" alt="MegaFiber Logo" className="w-60" />
+          <img src="/mega-fiber-logo.png" alt="MegaFiber Logo" className="w-60" />
           </Link>
       </div>
 
@@ -96,10 +96,10 @@ const Hero = ({ onOpenModal }: { onOpenModal: (type: string, data?: string) => v
       <div className="md:w-1/2 mb-12 md:mb-0">
         <h2 className="text-xl font-bold text-slate-800 tracking-wider mb-2 uppercase">Ultra Fast Entertainment</h2>
         <div className="flex items-baseline mb-4">
-          <h1 className="text-8xl md:text-[150px] font-black text-slate-900 leading-none tracking-tighter">500</h1>
+          <h1 className="text-8xl md:text-[150px] font-black text-slate-900 leading-none tracking-tighter">1000</h1>
           <span className="text-2xl md:text-3xl font-bold text-white bg-slate-800 px-4 py-2 ml-2 rounded-md">Mbps</span>
         </div>
-        <h3 className="text-2xl md:text-3xl font-bold text-slate-800 mb-8 uppercase">Starts From ₹499 Per Month</h3>
+        <h3 className="text-2xl md:text-3xl font-bold text-slate-800 mb-8 uppercase">Starts From ₹399 Per Month</h3>
         <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4">
           <button onClick={() => onOpenModal('contact')} className="bg-gradient-to-r from-emerald-500 to-teal-600 text-white px-8 py-3 rounded-md font-medium flex items-center justify-center hover:shadow-lg transition-all duration-500">
             Contact Now <ArrowRight size={18} className="ml-2" />
@@ -269,7 +269,7 @@ const Footer = () => (
         </div> */}
         {/* Mega<span className="text-emerald-500">Fiber</span> */}
          <Link to="/" className="flex bg-white rounded items-center text-2xl font-bold text-slate-900">
-          <img src="mega-fiber-logo.png" alt="MegaFiber Logo" className="w-60" />
+          <img src="/mega-fiber-logo.png" alt="MegaFiber Logo" className="w-60" />
           </Link>
       </div>
 
@@ -1482,7 +1482,8 @@ const LoadingScreen = () => {
         }}
         className="text-emerald-600"
       >
-        <Wifi size={80} strokeWidth={2} />
+        {/* <Wifi size={80} strokeWidth={2} /> */}
+          <img  src="/mega-fiber-logo_1.png" alt="MegaFiber Logo" className="w-60" />
       </motion.div>
     </motion.div>
   );
