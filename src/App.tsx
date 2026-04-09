bb1789cfcc1545b0860cfd99b49d52211188ce2c
vaastikthegreat <vaastikthegreat@gmail.com>
2026-04-09 18:17:15 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -96,7 +96,7 @@ const Hero = ({ onOpenModal }: { onOpenModal: (type: string, data?: string) => v
       <div className="md:w-1/2 mb-12 md:mb-0">
         <h2 className="text-xl font-bold text-slate-800 tracking-wider mb-2 uppercase">Ultra Fast Entertainment</h2>
         <div className="flex items-baseline mb-4">
-          <h1 className="text-8xl md:text-[150px] font-black text-slate-900 leading-none tracking-tighter">1000</h1>
+          <h1 className="text-8xl md:text-[150px] font-black text-slate-900 leading-none tracking-tighter">500</h1>
           <span className="text-2xl md:text-3xl font-bold text-white bg-slate-800 px-4 py-2 ml-2 rounded-md">Mbps</span>
         </div>
         <h3 className="text-2xl md:text-3xl font-bold text-slate-800 mb-8 uppercase">Starts From ₹399 Per Month</h3>
