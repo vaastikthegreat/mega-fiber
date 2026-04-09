8110b6de36ffc7b7f4bc1b44072deae58398cd73
vaastikthegreat <vaastikthegreat@gmail.com>
2026-04-09 18:18:23 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -99,7 +99,7 @@ const Hero = ({ onOpenModal }: { onOpenModal: (type: string, data?: string) => v
           <h1 className="text-8xl md:text-[150px] font-black text-slate-900 leading-none tracking-tighter">500</h1>
           <span className="text-2xl md:text-3xl font-bold text-white bg-slate-800 px-4 py-2 ml-2 rounded-md">Mbps</span>
         </div>
-        <h3 className="text-2xl md:text-3xl font-bold text-slate-800 mb-8 uppercase">Starts From ₹399 Per Month</h3>
+        <h3 className="text-2xl md:text-3xl font-bold text-slate-800 mb-8 uppercase">Starts From ₹499 Per Month</h3>
         <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4">
           <button onClick={() => onOpenModal('contact')} className="bg-gradient-to-r from-emerald-500 to-teal-600 text-white px-8 py-3 rounded-md font-medium flex items-center justify-center hover:shadow-lg transition-all duration-500">
             Contact Now <ArrowRight size={18} className="ml-2" />
